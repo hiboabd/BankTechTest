762f719c3e0728098965ccbf4c7614b7d9c3b242
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-06-15 11:39:46 +0100

    Failing test: make deposit function increments balance

--- BankTest/spec/AccountSpec.js
+++ BankTest/spec/AccountSpec.js
@@ -9,4 +9,12 @@ describe('Account', () => {
  it('initialises with a balance of 0', () => {
    expect(account.balance).toEqual(0);
  })
+
+ describe('makeDeposit', () => {
+   it('increments balance by 500', () => {
+     account.makeDeposit(500);
+     expect(account.balance).not.toEqual(0);
+     expect(account.balance).toEqual(500);
+   })
+ })
 })
