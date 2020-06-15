f3f04a46da9d6fa9cc9a672483ea923128e9a5f0
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-06-15 14:49:04 +0100

    Tests pass: view statement prints account transaction history

--- BankTest/src/Account.js
+++ BankTest/src/Account.js
@@ -9,12 +9,24 @@ class Account {
   makeDeposit(amount, date=new Date()){
     this.balance += amount;
     date = date.toLocaleDateString() //formatting date to 'dd/mm/yyyy' format
-    this.accountHistory.unshift({date: date, credit: amount, debit: " ", balance: this.balance})
+    this.accountHistory.unshift({date: date, credit: amount.toFixed(2), debit: " ", balance: this.balance.toFixed(2)})
   }
 
   makeWithdrawal(amount, date=new Date()){
     this.balance -= amount;
     date = date.toLocaleDateString() //formatting date to 'dd/mm/yyyy' format
-    this.accountHistory.unshift({date: date, credit: " ", debit: amount, balance: this.balance})
+    this.accountHistory.unshift({date: date, credit: " ", debit: amount.toFixed(2), balance: this.balance.toFixed(2)})
+  }
+
+  viewStatement(){
+    var statement = `date || credit || debit || balance`
+    this.accountHistory.forEach((object, i) => {
+      statement += `\n`
+      for(const property in object){
+        statement += `${object[property]} || `
+      }
+      statement = statement.slice(0, statement.length - 3);
+    });
+    return statement
   }
 };
