899adab2837edd6be7164c84c05035ca3cfea073
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-06-15 15:13:07 +0100

    tests pass: printer class prints statements

---
+++ BankTest/src/Printer.js
@@ -0,0 +1,16 @@
+class Printer {
+  constructor(){
+    this.tableHeader = `date || credit || debit || balance`
+  }
+
+  printStatement(accountHistory){
+    var statement = this.tableHeader
+    accountHistory.forEach((transaction) => {
+      statement += `\n`
+      for(const property in transaction){
+        statement += `${transaction[property]} || `
+      }
+      statement = statement.slice(0, statement.length - 3);
+    });
+  }
+}
