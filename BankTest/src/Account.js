48bd2bc723f94009eacdce17edfda3589958f858
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-06-15 15:04:23 +0100

    refactoring: format date method, table header extracted and improved variable naming

--- BankTest/src/Account.js
+++ BankTest/src/Account.js
@@ -4,29 +4,33 @@ class Account {
   constructor() {
     this.balance = 0;
     this.accountHistory = [];
+    this.tableHeader = `date || credit || debit || balance`
+  }
+
+  formatDate(date){
+    return date.toLocaleDateString() //formatting date to 'dd/mm/yyyy' format
   }
 
   makeDeposit(amount, date=new Date()){
     this.balance += amount;
-    date = date.toLocaleDateString() //formatting date to 'dd/mm/yyyy' format
+    date = this.formatDate(date);
     this.accountHistory.unshift({date: date, credit: amount.toFixed(2), debit: " ", balance: this.balance.toFixed(2)})
   }
 
   makeWithdrawal(amount, date=new Date()){
     this.balance -= amount;
-    date = date.toLocaleDateString() //formatting date to 'dd/mm/yyyy' format
+    date = this.formatDate(date);
     this.accountHistory.unshift({date: date, credit: " ", debit: amount.toFixed(2), balance: this.balance.toFixed(2)})
   }
 
   viewStatement(){
-    var statement = `date || credit || debit || balance`
-    this.accountHistory.forEach((object, i) => {
+    var statement = this.tableHeader
+    this.accountHistory.forEach((transaction) => {
       statement += `\n`
-      for(const property in object){
-        statement += `${object[property]} || `
+      for(const property in transaction){
+        statement += `${transaction[property]} || `
       }
       statement = statement.slice(0, statement.length - 3);
     });
-    return statement
   }
 };
