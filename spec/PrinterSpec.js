00b648a33fe732d2a4a748fdae27bc6d11acb102
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-06-17 15:21:49 +0100

    Failing test: print statement now takes balance record argument and returns statement

--- spec/PrinterSpec.js
+++ spec/PrinterSpec.js
@@ -1,16 +1,30 @@
 describe('Printer', () => {
  'use strict'
  var printer;
+ var transaction1
+ var transaction2
  var accountHistory;
+ var balanceRecord;
 
  beforeEach(() => {
+   transaction1 = {
+     date: '15/06/2020',
+     amount: 1500,
+     type: 'Deposit'
+   }
+   transaction2 = {
+     date: '15/06/2020',
+     amount: 500,
+     type: 'Withdrawal'
+   }
    printer = new Printer();
-   accountHistory = [{date: '15/06/2020', credit: " ", debit: "500.00", balance: "1000.00"}, {date: '15/06/2020', credit: "1500.00", debit: " ", balance: "1500.00"}]
+   accountHistory = [transaction1, transaction2]
+   balanceRecord = [1500, 1000]
  });
 
   describe('printStatement', () => {
     it('prints statement into table', () => {
-      expect(printer.printStatement(accountHistory)).toBe(`date || credit || debit || balance\n15/06/2020 ||   || 500.00 || 1000.00 \n15/06/2020 || 1500.00 ||   || 1500.00 `)
+      expect(printer.printStatement(accountHistory, balanceRecord)).toBe(`date || credit || debit || balance\n15/06/2020 ||   || 500.00 || 1000.00\n15/06/2020 || 1500.00 ||   || 1500.00`)
     });
   })
 })
